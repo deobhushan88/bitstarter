a9bc6dd3cb51dff170ca6234c61480e8dc31c1c3
Bhushan Deo <deobhushan88@gmail.com>
2013-09-29 02:42:57 +0000

    trying to fix again2

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 var fs = require('fs');
-var buf = new Buffer(256);
-buf.write(buf.toString(fs.readFileSync('\index.html')));
+var buf = new Buffer(25600);
+buf.write(buf.toString(fs.readFileSync('index.html')));
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
